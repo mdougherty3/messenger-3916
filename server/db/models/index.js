89c25d80535a8e7007c408f554b1bfa74a870f97
mike <mdougherty@topnotchinc.com>
2021-09-23 20:43:52 -0400

    Remove unnecessary association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,7 +4,6 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
 Conversation.belongsTo(User, { as: "user1" });
 Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation);

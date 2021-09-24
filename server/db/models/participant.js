d3431c3901284fb04e26df1806bca4a765c168a9
mike <mdougherty@topnotchinc.com>
2021-09-23 23:10:14 -0400

    Remove readStatus from Message model, add lastReadMessageId to Participant model

--- server/db/models/participant.js
+++ server/db/models/participant.js
@@ -1,6 +1,16 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const Message = require("./message");
 
-const Participant = db.define("participant", {});
+const Participant = db.define("participant", {
+  lastReadMessageId: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    references: {
+        model: Message,
+        key: 'id'
+    }
+  },
+});
 
 module.exports = Participant;
